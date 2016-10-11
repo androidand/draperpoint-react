c7e653817c70f890bf12d3cfa0e73b331469da66
Andreas Sylvan <tantonet@gmail.com>
2016-10-11 11:48:19 +0200

    Added initial Prev Next for Player
    Needs a lot more work. =)

--- src/Player.js
+++ src/Player.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Audio from 'react-howler';
+import Yl from 'react-howler';
 import raf from 'raf'; // requestAnimationFrame polyfill
 
 class Player extends Component {
@@ -7,11 +7,17 @@ class Player extends Component {
   constructor (props) {
     super(props)
 
+        this.files = [
+        'https://s3-eu-west-1.amazonaws.com/thanksforthe-media/160807+You+Want+Me+To+Go+Let+It+Go+Jam.mp3',
+        'https://s3-eu-west-1.amazonaws.com/thanksforthe-media/Rock+It+tonight....m4a'
+        ];
+
     this.state = {
       playing: true,
       loaded: false,
       loop: false,
-      mute: false
+      mute: false,
+      current: this.files[0]
     }
     this.handleToggle = this.handleToggle.bind(this)
     this.handleOnLoad = this.handleOnLoad.bind(this)
@@ -22,11 +28,8 @@ class Player extends Component {
     this.handleMuteToggle = this.handleMuteToggle.bind(this)
 
     this.handleOnNext = this.handleOnNext.bind(this)
+    this.handleOnPrev = this.handleOnPrev.bind(this)
 
-    this.files = [
-        'https://s3-eu-west-1.amazonaws.com/thanksforthe-media/160807+You+Want+Me+To+Go+Let+It+Go+Jam.mp3',
-        'https://s3-eu-west-1.amazonaws.com/thanksforthe-media/Rock+It+tonight....m4a'
-        ];
   }
 //Alotta functions
 componentWillUnmount () {
@@ -62,13 +65,23 @@ handleOnEnd () {
 
 
   handleOnPrev () {
-    this.prev();
+    console.log("Length: " +  this.files.length);
+    console.log("Index: " + this.files.indexOf(this.state.current));
+    
+    this.setState({
+      current: this.files[0]
+    })
+    
   }
 
   handleOnNext () {
-    console.log( this.getMethods(this) );
+    console.log("Length: " +  this.files.length);
+    console.log("Index: " + this.files.indexOf(this.state.current));
     
-    //this.next();
+    this.setState({
+      current: this.files[1]
+    })
+    
   }
 
 
@@ -106,7 +119,7 @@ getMethods(obj)
 {
     var res = [];
     for(var m in obj) {
-        if(typeof obj[m] == "function") {
+        if(typeof obj[m] === "function") {
             res.push(m)
         }
     }
@@ -128,8 +141,8 @@ getMethods(obj)
 
 
 
-  	  <Audio
-        src={this.files}
+  	  <Yl
+        src={this.state.current}
         playing={this.state.playing}
         onLoad={this.handleOnLoad}
         onPlay={this.handleOnPlay}
@@ -140,6 +153,10 @@ getMethods(obj)
       />
 
   	 <div id="controlPanel">
+
+     <button onClick={this.handleOnPrev}>
+      {(this.state.playing) ? 'Prev' : 'Prev!'}
+     </button>
      
       <button onClick={this.handleToggle}>
         {(this.state.playing) ? 'Pause' : 'Play'}
@@ -156,6 +173,7 @@ getMethods(obj)
           {(this.state.seek !== undefined) ? this.state.seek.toFixed(2) : 'NaN'}
           {' / '}
           {(this.state.duration) ? this.state.duration.toFixed(2) : 'NaN'}
+
         </p>
 
         <label>
